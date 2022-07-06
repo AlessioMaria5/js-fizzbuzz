92e42c01f0810f762cdf62991f7ae38afb7a1514
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-06 23:46:04 +0200

    numero print ?

--- BONUS 1/js/main.js
+++ BONUS 1/js/main.js
@@ -1,41 +1,49 @@
+const interactive = document.getElementById('btn');
+const numPrint = document.getElementById('numPrint');
 
+    interactive.addEventListener ('click',
+    function() {
 
-// ul.append(li);
+        let i = (numPrint.value);
+        console.log(i);
+
+    });
 
 const cont_in = document.getElementById('container');
 
-    for (let i=1; i<=100; i++) {
+    for (let i=1; i<=(1+numPrint.value); i++) {
         
         if (i%15 == 0) {
             cont_in.innerHTML += ('<div class="box" style="background-color:#F53364">' + 'fizzbuzz' + '</div>');
-            console.log('i');
+            
         }
 
         else if (i%5 == 0) {
             cont_in.innerHTML += ('<div class="box" style="background-color:#FFCA5D">' + 'Buzz' + '</div>');
-            console.log('i');
+           
         }
 
         else if (i%3 == 0) {
             cont_in.innerHTML += ('<div class="box" style="background-color:#00D396">' + 'Fizz' + '</div>');
-            console.log('i');
+            
         }
 
 
         else {
             cont_in.innerHTML += ('<div class="box" style="background-color:#007EA8">' + i + '</div>');
-            console.log('i');
+           
         }
+        console.log(i);
     }
 
-
-const containerPlus = document.getElementById('container-plus');
-const NuovoElemento = document.createElement('section');
-
-
-
-containerPlus.append(NuovoElemento);
-const string = NuovoElemento.append('That is' + cont_in);
+    const containerPlus = document.getElementById('container-plus');
+    const NuovoElemento = document.createElement('section');
+    
+    
+    
+    containerPlus.append(NuovoElemento);
+    NuovoElemento.append('That is' + cont_in);
+    NuovoElemento.append(`That is${NuovoElemento}`);
 
 
 
