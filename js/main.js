d8c88fca506f516a45e73dd03c06b0cf8cd74e9c
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-06 22:02:53 +0200

    Completo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,46 @@
+const cont_in = document.getElementById('container');
 
     for (let i=1; i<=100; i++) {
+        
+        if (i%15 == 0) {
+            cont_in.innerHTML += ('<div class="box" style="background-color:#F53364">' + 'fizzbuzz' + '</div>');
+            console.log('i');
+        }
+
+        else if (i%5 == 0) {
+            cont_in.innerHTML += ('<div class="box" style="background-color:#FFCA5D">' + 'Buzz' + '</div>');
+            console.log('i');
+        }
+
+        else if (i%3 == 0) {
+            cont_in.innerHTML += ('<div class="box" style="background-color:#00D396">' + 'Fizz' + '</div>');
+            console.log('i');
+        }
 
 
-        container.innerHTML += ('<div class="box">' + i + '</div>');
-        console.log(i);
+        else {
+            cont_in.innerHTML += ('<div class="box" style="background-color:#007EA8">' + i + '</div>');
+            console.log('i');
+        }
+          
+
+        
+
+        
+      
+
+
+
+        // if (i%5 == 0) {
+
+        //     cont_in.innerHTML += ('<div class="box" style="background-color: red">' + i + '</div>');
+        // } else {
+
+        //     cont_in.innerHTML += ('<div class="box">' + i + '</div>');
+        // }  
     }
+
+
 
 
 
